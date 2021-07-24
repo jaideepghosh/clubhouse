d185fa4b1cd58c943f11dfa3bf8112f77da8f4a2
jaideepghosh <er.jaideepghosh@gmail.com>
2021-07-24 17:09:05 +0530

    feat:Added clerkProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { useRouter } from "next/router";
+
+const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <ClerkProvider
+      frontendApi={clerkFrontendApi}
+      navigate={(to) => router.push(to)}
+    >
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
