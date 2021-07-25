45394c9b7011be22b9d5d2531ef194481d4d67e1
jaideepghosh <er.jaideepghosh@gmail.com>
2021-07-25 18:37:22 +0530

    feat:Added register component.

---
+++ pages/register/[[...index]].js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SignUp } from "@clerk/clerk-react";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <SignUp path="/register" routing="path" />
+      </div>
+    </Container>
+  );
+}
