c08540ec5f557ee72e7c4bc6d91916ec8ece64df
jaideepghosh <er.jaideepghosh@gmail.com>
2021-07-25 18:36:43 +0530

    feat:Included header and home component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import HomeBody from "../components/Home";
+
 export default function Home() {
   return (
-    <></>
-  )
+    <div className="flex justify-center mt-80">
+      <Head>
+        <title>Clubhouse</title>
+        <meta name="description" content="Open source knowledgebase." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <HomeBody />
+    </div>
+  );
 }
