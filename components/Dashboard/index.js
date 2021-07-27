2570318e19e6a2c2b19a467cff120763c18f5e73
jaideepghosh <er.jaideepghosh@gmail.com>
2021-07-27 23:47:11 +0530

    feat:Added dashboard dummy template.

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { useUser } from "@clerk/clerk-react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
 const GuestHome = () => {
   const { firstName } = useUser();
-  return <>Welcome {firstName}</>;
+  const userData = useUser();
+  return (
+    <Container component="main" maxWidth="md" style={{ marginTop: "20px" }}>
+      <Typography component="h1" gutterBottom variant="h3" align="center">
+        👋 Hey, {firstName}
+      </Typography>
+      <Typography gutterBottom variant="subtitle1" align="center">
+        Find the trending topics and course contents below.
+      </Typography>
+    </Container>
+  );
 };
 
 export default GuestHome;
